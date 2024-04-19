964222472f2bbc1bbe50394de2d53a45c38409ca
elisemomo <mukomelise@gmail.com>
2024-04-18 22:23:01 -0400

    terraform file updated

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "elisemomo" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-east-2"
   }
 }
